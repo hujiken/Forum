6e9f4228c55d6f63ad49e3b01560e01338a1e69a
hujiken <phamhuycntt@gmail.com>
2018-05-15 22:22:21 +0700

    2018/05/15_home: validate, account

---
+++ Forum/src/main/java/model/Account.java
@@ -0,0 +1,73 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="account")
+public class Account {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Size(min = 5, max = 50)
+	@NotNull
+	@Column(name = "username", nullable = false)
+	private String username;
+	
+	@Email
+	@NotNull
+	@Column(name = "email", nullable = false)
+	private String email;
+	
+	@Size(min = 5, max = 50)
+	@NotNull
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", username=" + username + ", email=" + email + ", password=" + password + "]";
+	}
+	
+}
